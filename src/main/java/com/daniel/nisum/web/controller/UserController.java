2a6583cb6ffbeb82c4beeb1ce1a256b451a87f4a
Daniel Florez <daniel.florez9612@gmail.com>
2020-10-06 16:37:39 -0500

    feat created status for user registration

--- src/main/java/com/daniel/nisum/web/controller/UserController.java
+++ src/main/java/com/daniel/nisum/web/controller/UserController.java
@@ -3,8 +3,10 @@ package com.daniel.nisum.web.controller;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.daniel.nisum.persistence.model.User;
@@ -18,6 +20,7 @@ public class UserController {
 	private IUserService userService;
 	
 	@PostMapping("/users")
+	@ResponseStatus(code = HttpStatus.CREATED)
 	public User register(@RequestBody @Valid UserDto user) {
 		return userService.registerUser(user);
 	}
